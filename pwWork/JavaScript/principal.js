26fa7715b7e8013f2e3b4ec60507d7fd8d205a53
tassinarilucas08 <lucastassinari.ch023@academico.ifsul.edu.br>
2025-05-16 07:52:50 -0300

    test

--- pwWork/JavaScript/principal.js
+++ pwWork/JavaScript/principal.js
@@ -3,3 +3,40 @@ function scrollToProdutos() {
     produtosSection.scrollIntoView({ behavior: 'smooth' });
   }
   
+    let hideTimeout;
+
+    function showModal() {
+      clearTimeout(hideTimeout);
+      document.getElementById('modal').classList.add('active');
+    }
+
+    function startHideModal() {
+      hideTimeout = setTimeout(() => {
+        document.getElementById('modal').classList.remove('active');
+      }, 150);
+    }
+
+    function hideModal() {
+      document.getElementById('modal').classList.remove('active');
+    }
+
+    function cancelHideModal() {
+      clearTimeout(hideTimeout);
+    }
+
+    let motherInformationsClick = document.querySelector("#mother-informations");
+    motherInformationsClick.addEventListener('click', ()=>{
+        showModal();
+    });
+
+    function filterProducts(category) {
+  const products = document.querySelectorAll('.product-card');
+  products.forEach(product => {
+    if (category === 'all') {
+      product.style.display = 'block';
+    } else {
+      product.style.display = product.classList.contains(category) ? 'block' : 'none';
+    }
+  });
+  scrollToProdutos();
+}
