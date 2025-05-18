75dba1e80fa67dde326837874406e026f863ab7c
Matheus551-coder <matheusgoncalves.ch006@academico.ifsul.edu.br>
2025-05-17 21:30:33 -0300

    test

--- pwWork/front/JavaScript/compras.js
+++ pwWork/front/JavaScript/compras.js
@@ -1,4 +1,4 @@
-let voltar = document.querySelector("#btn-voltar");
+let voltar = document.querySelector(".btn-voltar");
 
 voltar.addEventListener('click', ()=>{
     window.location.href = "../html/principal.html";
