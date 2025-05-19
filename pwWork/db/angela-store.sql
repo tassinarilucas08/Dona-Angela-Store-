6cd32c2d6b91a0a538c9550f7a3334de4516e8cb
Matheus551-coder <matheusgoncalves.ch006@academico.ifsul.edu.br>
2025-05-19 17:43:13 -0300

    test

--- pwWork/db/angela-store.sql
+++ pwWork/db/angela-store.sql
@@ -3,18 +3,18 @@ USE angela_store;
 
 CREATE TABLE IF NOT EXISTS address (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    street VARCHAR(80) NOT NULL,
+    street VARCHAR(255) NOT NULL,
     number INT NOT NULL,
-    complement VARCHAR(60),
-    city VARCHAR(100) NOT NULL,
-    state VARCHAR(100) NOT NULL,
-    zip_code VARCHAR(20) NOT NULL
+    complement VARCHAR(255),
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    zip_code VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS seller (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(80) NOT NULL,
-    email VARCHAR(120) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     address_id INT,
     FOREIGN KEY (address_id)
@@ -23,8 +23,8 @@ CREATE TABLE IF NOT EXISTS seller (
 
 CREATE TABLE IF NOT EXISTS client (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(80) NOT NULL,
-    email VARCHAR(120) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     address_id INT,
     FOREIGN KEY (address_id)
@@ -34,34 +34,18 @@ CREATE TABLE IF NOT EXISTS client (
 CREATE TABLE IF NOT EXISTS product (
     id INT AUTO_INCREMENT PRIMARY KEY,
     seller_id INT NOT NULL,
-    name VARCHAR(60) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     price DOUBLE NOT NULL,
     image INT,
     quantity INT NOT NULL,
-    status VARCHAR(20),
+    status VARCHAR(255),
     FOREIGN KEY (seller_id)
 		REFERENCES seller(id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS shopBasket (
+CREATE TABLE IF NOT EXISTS faq (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    client_id INT NULL,
-    total DOUBLE,
-    FOREIGN KEY (client_id)
-		REFERENCES client(id)
+    question VARCHAR(255) NOT NULL,
+    answer VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS shopBasket_item (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    basket_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    price DOUBLE NOT NULL,
-    FOREIGN KEY (basket_id)
-		REFERENCES shopBasket(id)
-        ON DELETE CASCADE,
-	FOREIGN KEY (product_id)
-		REFERENCES product(id)
-) ENGINE=InnoDB;
-delete "angela_store";
